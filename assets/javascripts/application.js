1af2eefd18225eb0d1fec5813708bcc13d7f5172
Zac Stewart <zgstewart@gmail.com>
2012-07-01 23:47:28 -0400

    Add sidebar and salary figures

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,6 +2,10 @@ window.puts = function () {
   var log = Function.prototype.bind.call(console.log, console);
   log.apply(window.console, arguments);
 };
+Number.prototype.toCurrency = function(c, d, t){
+  var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
 var
   Datum = Backbone.Model.extend(),
   Dataset = Backbone.Collection.extend({
@@ -72,18 +76,33 @@ var
         .on('mouseover', function (d, i) { d3.select('text#label_' + i).attr('display', 'inline') })
         .on('mouseout', function (d, i) { d3.select('text#label_' + i).attr('display', 'none') });
       arcs.append("text")
-        .attr("cx", 360)
-        .attr("cy", 360)
+        .attr("cx", this.width / 2)
+        .attr("cy", this.height / 2)
         .attr("text-anchor", "middle")
         .attr("display", "none")
         .attr('id', function (d, i) { return 'label_' + i; })
-        .text(function(d, i) { return data.keys()[i]; });
+        .text(function(d, i) { return '$' + d.value.toCurrency(2, '.', ',') + ' ' + data.keys()[i] });
       return this;
+    }
+  }),
+  SidebarView = Backbone.View.extend({
+    template: function () { return _.template($('#sidebar_template').text()); },
+    render: function () {
+      this.$el.html(this.template());
+      return this;
+    },
+    events: {
+      'click': 'navigateTo'
+    },
+    navigateTo: function (e) {
+      e.preventDefault();
     }
   });
 
 $(function () {
   d3.csv('data/salary_travel_2011.csv', function (pd) {
+    window.sidebarView = new SidebarView();
+    window.sidebarView.render().$el.appendTo('#sidebar');
     window.dataset = new Dataset(pd);
     window.baseView = new BaseView({collection: window.dataset});
     $('#content').append(baseView.render().$el);
